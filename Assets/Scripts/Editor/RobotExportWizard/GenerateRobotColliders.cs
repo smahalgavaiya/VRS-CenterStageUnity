24df58f3c90f3d651f9636040c4d4a39fd8fa2e5
jasoben <jasoben@gmail.com>
2022-06-25 15:42:12 -0400

    Collider Generator Working

--- Assets/Scripts/Editor/RobotExportWizard/GenerateRobotColliders.cs
+++ Assets/Scripts/Editor/RobotExportWizard/GenerateRobotColliders.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-public class GenerateWheelColliders : EditorWindow
+public class GenerateRobotColliders : EditorWindow
 {
     [SerializeField]
     private GameObject robotParent;
     GameObject wheelsParent;
     private GameObject wheelCollidersParent;
 
+    [SerializeField]
+    private bool hasMecanumWheels;
+
     public void CreatePopup()
     {
-        EditorWindow window = EditorWindow.CreateInstance<GenerateWheelColliders>();
+        EditorWindow window = EditorWindow.CreateInstance<GenerateRobotColliders>();
         EditorStyles.label.wordWrap = true;
         window.Show();
     }
@@ -26,13 +29,28 @@ public class GenerateWheelColliders : EditorWindow
         EditorGUILayout.Space();
         EditorGUILayout.Space();
 
-        if (GUILayout.Button("Generate Wheel Colliders"))
+        hasMecanumWheels = (bool)EditorGUILayout.Toggle("Has Mecanum Wheels?", hasMecanumWheels);
+
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+
+        if (GUILayout.Button("Generate Robot Colliders"))
         {
             if (robotParent == null)
             {
                 ShowNotification(new GUIContent("You need to select \n the Robot Parent (above) \n for the root of the robot"), 5);
                 return;
             }
+
+            if (robotParent.GetComponent<Rigidbody>() == null)
+            {
+                Rigidbody robotRB = robotParent.AddComponent<Rigidbody>();
+                robotRB.mass = 1000;
+            }
+
             List<GameObject> wheels = new List<GameObject>();
 
             bool wheelsParentCreated = false;
@@ -48,37 +66,80 @@ public class GenerateWheelColliders : EditorWindow
                 wheelCollidersParent = new GameObject("WheelColliders");
                 wheelCollidersParent.transform.SetParent(robotParent.transform);
                 wheelCollidersParent.transform.rotation = robotParent.transform.rotation;
+
+                MoveWheelsToWheelsParent();
             }
 
-            MoveWheelsToWheelsParent();
+            if (wheelsParentCreated)
+            {
+                ShowNotification(new GUIContent("It looks like you \n already made wheel colliders \n " +
+                    "if you want to restart \n delete the WheelColliders \n and WheelModels \n objects from " +
+                    "your robot hierarchy."), 5);
+                return;
+            }
+
+            if (hasMecanumWheels && robotParent.GetComponent<DriveReceiverMecanum>() == null)
+            {
+                robotParent.AddComponent<DriveReceiverMecanum>();
+            }
         }
     }
 
     private void MoveWheelsToWheelsParent()
     {
+        bool wheelsNamedCorrectly = false;
+
         for (int i = 0; i < robotParent.transform.childCount; i++)
         {
             GameObject robotPart = robotParent.transform.GetChild(i).gameObject;
+
+            if (robotPart.GetComponent<MeshCollider>() == null)
+            {
+                MeshCollider meshCollider = robotPart.AddComponent<MeshCollider>();
+                meshCollider.convex = true;
+            }
+
+            robotPart.layer = LayerMask.NameToLayer("Robot");
+
             if (
                 (robotPart.name.Contains("wheel") || robotPart.name.Contains("Wheel"))
                 && !robotPart.name.Contains("Colliders") 
                 && !robotPart.name.Contains("Models")
                )
             {
+                wheelsNamedCorrectly = true;
+
+                // We do not want the wheels to have mesh colliders, as they use wheel colliders
+                // however we globally added mesh colliders to every part in the for loop above,
+                // so we need to now delete it.
+                if (robotPart.GetComponent<MeshCollider>() != null)
+                {
+                    DestroyImmediate(robotPart.GetComponent<MeshCollider>());
+                }
+
                 GameObject newWheel = Instantiate(robotPart);
                 GameObject newWheelCollider = Instantiate(robotPart);
+
+                newWheel.name = newWheel.name.Remove(newWheel.name.IndexOf("("), 7);
+                newWheelCollider.name = newWheel.name + "_WheelCollider";
+                newWheel.name = newWheel.name + "_WheelModel";
 
                 MatchLocRotScale(robotPart, newWheel);
                 MatchLocRotScale(robotPart, newWheelCollider);
 
                 newWheel.AddComponent<DriveReceiverForTransformRotate>();
                 WheelCollider wheelCollider = newWheelCollider.AddComponent<WheelCollider>();
+                wheelCollider.suspensionDistance = .1f;
                 newWheelCollider.GetComponent<Renderer>().enabled = false;
 
                 newWheel.transform.SetParent(wheelsParent.transform);
                 newWheelCollider.transform.SetParent(wheelCollidersParent.transform);
-                //robotPart.SetActive(false);
+                robotPart.SetActive(false);
             }
+        }
+        if (!wheelsNamedCorrectly)
+        {
+            ShowNotification(new GUIContent("You need to name \n the wheels with the word \n \"wheel\" in the object name"), 5);
         }
     }
 
@@ -94,11 +155,11 @@ public class GenerateWheelColliders : EditorWindow
         for (int i = 0; i < robotParent.transform.childCount; i++)
         {
             GameObject robotPart = robotParent.transform.GetChild(i).gameObject;
-            if (robotPart.name == "WheelsModels")
+            if (robotPart.name == "WheelModels")
             {
                 wheelsParentCreated = true;
+                break;
             } else wheelsParentCreated = false;
-
         }
 
         return wheelsParentCreated;
