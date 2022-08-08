77f3e8f38fcfa7492dd5415b446c9bf451c9d246
ChrisAshtear <chrisashtear@gmail.com>
2022-08-08 13:17:18 -0400

    imported CK package.

--- Assets/Scripts/Globals/ScriptableObjects/GlobalBool.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Globals/Global Bool")]
-public class GlobalBool : ScriptableObject
-{
-    public bool boolValue;
-}
--- Assets/Scripts/Goals and Scoring/ICustomGoalEvents.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-internal interface ICustomGoalEvents
-{
-    public void DoCustomOnEvent(Object objectToPass);
-    public void DoCustomOffEvent(Object objectToPass);
-}
