0c74a704ecc1197b32ebf677e6936daf43daa38e
jasoben <jasoben@gmail.com>
2022-08-20 09:38:15 -0400

    added rotation

--- Assets/Scripts/Test/ArtWheelTest.cs
+++ Assets/Scripts/Test/ArtWheelTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,17 +6,38 @@ using UnityEngine.InputSystem;
 
 public class ArtWheelTest : MonoBehaviour
 {
-    private float motionFB;
-    private float motionRL;
+    private float motionFB, motionRL, rotationRL;
 
-    [SerializeField] List<GameObject> wheels;
+    [SerializeField] GameObject robotBody, frontLeftWheel, 
+        backLeftWheel, frontRightWheel, backRightWheel;
+
+    ArticulationBody frontLeftArtBod, backLeftArtBod, frontRightArtBod, backRightArtBod;
+    
+    List<GameObject> wheels;
+    List<ArticulationBody> wheelArticulationBodies;
 
     [SerializeField] float coefficient;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        wheels = new List<GameObject>();
+        wheelArticulationBodies = new List<ArticulationBody>();
+
+        wheels.Add(frontRightWheel);
+        wheels.Add(frontLeftWheel);
+        wheels.Add(backLeftWheel);
+        wheels.Add(backRightWheel);
+
+        foreach (GameObject wheel in wheels)
+        {
+            wheelArticulationBodies.Add(wheel.GetComponent<ArticulationBody>());
+        }
+
+        frontRightArtBod = frontRightWheel.GetComponent<ArticulationBody>();
+        frontLeftArtBod = frontLeftWheel.GetComponent<ArticulationBody>();
+        backRightArtBod = backRightWheel.GetComponent<ArticulationBody>();
+        backLeftArtBod = backLeftWheel.GetComponent<ArticulationBody>();
     }
 
     // Update is called once per frame
@@ -27,16 +49,28 @@ public class ArtWheelTest : MonoBehaviour
     {
         motionFB = value.Get<Vector2>().y;
         motionRL = value.Get<Vector2>().x;
-
+    }
+    public void OnRotation(InputValue value)
+    {
+        rotationRL = value.Get<float>();
     }
     private void FixedUpdate()
     {
-        foreach(GameObject wheel in wheels)
+        foreach(ArticulationBody wheelArtBod in wheelArticulationBodies)
         {
-            ArticulationBody body = wheel.GetComponent<ArticulationBody>();
-            body.AddTorque(wheel.transform.parent.right * motionFB * coefficient);
-            body.AddTorque(wheel.transform.parent.forward * -motionRL * coefficient);
-
+            wheelArtBod.AddTorque(robotBody.transform.right.normalized * motionFB * coefficient);
+            wheelArtBod.AddTorque(robotBody.transform.forward.normalized * -motionRL * coefficient);
         }
+
+        RotateDirectionally(frontLeftArtBod, backLeftArtBod, 1);
+        RotateDirectionally(frontRightArtBod, backRightArtBod, -1);
+    }
+
+    private void RotateDirectionally(ArticulationBody artBodOne, 
+        ArticulationBody artBodTwo, 
+        int direction)
+    {
+        artBodOne.AddTorque(robotBody.transform.right.normalized * rotationRL * coefficient * direction);
+        artBodTwo.AddTorque(robotBody.transform.right.normalized * rotationRL * coefficient * direction);
     }
 }
