81b3e9280411e088953e6d5085dcf31164d1e951
jasoben <jasoben@gmail.com>
2022-08-20 14:09:26 -0400

    added signal zone scoring for parked robots

---
+++ Assets/Scripts/Goals and Scoring/Custom/CheckRobotColor.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckRobotColor : MonoBehaviour, ICustomGoalChecker
+{
+    GoalZoneBaseData goalZoneBaseData;
+    GoalZoneScoreLink goalZoneScoreLink;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        goalZoneBaseData = GetComponent<GoalZoneBaseData>();
+        goalZoneScoreLink = GetComponent<GoalZoneScoreLink>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void DoCustomCheck()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void DoCustomCheck(GameObject objectToCheck)
+    {
+        if (goalZoneBaseData.scoreZoneColor ==
+            objectToCheck.GetComponentInParent<ScoreObjectTypeLink>().LastTouchedTeamColor)
+            goalZoneScoreLink.OptionalBoolValue = true;
+
+    }
+}
