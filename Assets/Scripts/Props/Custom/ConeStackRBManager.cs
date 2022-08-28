b9de76554fb98dd7f70d141d3e4bc438a6fbeb29
jasoben <jasoben@gmail.com>
2022-08-27 21:49:53 -0400

    fixed beacon models, cone stack knockover bug

--- Assets/Scripts/Props/Custom/ConeStackRBManager.cs
+++ Assets/Scripts/Props/Custom/ConeStackRBManager.cs
@@ -8,17 +8,29 @@ public class ConeStackRBManager : MonoBehaviour
     [SerializeField] GameObject physicalCones;
     [SerializeField] GameObject dummyCones;
 
+    List<GameObject> physicalConeObjects = new List<GameObject>();
     public GameObject PhysicalCones { get => physicalCones; }
 
     // Start is called before the first frame update
     void Start()
     {
         PhysicalCones.SetActive(false);
+
+        for(int i = 0; i < physicalCones.transform.childCount; i++)
+        {
+            physicalConeObjects.Add(physicalCones.transform.GetChild(i).gameObject);
+        }
     }
 
     public void SwitchOnPhysicalCones()
     {
         PhysicalCones.SetActive(true);
+
+        foreach(GameObject obj in physicalConeObjects)
+        {
+            obj.transform.SetParent(transform.parent);
+        }
+
         dummyCones.SetActive(false);
     }    
 
