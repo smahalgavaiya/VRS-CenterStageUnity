713143f2b346c834e4ce7d40edf69f20e5f3e394
ChrisAshtear <chrisashtear@gmail.com>
2022-08-29 15:40:54 -0400

    laser for both robots

---
+++ Assets/AdjustLaser.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustLaser : MonoBehaviour
+{
+    public LayerMask layerMask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray r = new Ray(transform.position, transform.TransformDirection(Vector3.forward));
+        if (Physics.Raycast(r,out hit,10,layerMask,QueryTriggerInteraction.Ignore))
+        {
+            if (hit.collider.isTrigger) { return; }
+            Debug.DrawRay(transform.position, hit.point);
+            float distance = hit.distance;
+            Vector3 newScale = transform.localScale;
+            newScale.z = distance * 0.3f;
+            transform.localScale = newScale;
+            Debug.Log(hit.collider.name);
+        }
+    }
+}
