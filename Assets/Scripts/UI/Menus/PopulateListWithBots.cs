d6c3dfc232a99808db96b005efb1df17736f5103
ChrisAshtear <chrisashtear@gmail.com>
2022-11-13 16:46:22 -0500

    sort of working bot select in multi

---
+++ Assets/Scripts/UI/Menus/PopulateListWithBots.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof(OptionListImage))]
+public class PopulateListWithBots : MonoBehaviour
+{
+    OptionListImage listObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        listObj = GetComponent<OptionListImage>();
+        List<BotData> bots = BuiltInBots.GetBotList();
+        listObj.options = bots.Select(item => item.name).ToList();
+        listObj.images = bots.Select(item => item.img).ToList();
+        listObj.ChangeOption(-1);
+    }
+
+}
