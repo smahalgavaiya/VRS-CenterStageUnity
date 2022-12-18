15aa405766fffefab93d163ca815a116bd9107e9
ChrisAshtear <chrisashtear@gmail.com>
2022-12-18 11:38:26 -0500

    some bug fixes

--- Assets/Scripts/Goals and Scoring/Custom/ReleaseObjectInField.cs
+++ Assets/Scripts/Goals and Scoring/Custom/ReleaseObjectInField.cs
@@ -1,13 +1,23 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class ReleaseObjectInField : MonoBehaviour
+public class ReleaseObjectInField : MonoBehaviourPunCallbacks
 {
     public List<GameObject> Objects;
     public List<GameObject> Location;
     public bool cloneObject = false;
     TeamColor team;
+    PhotonView view;
+
+    private void Start()
+    {
+        view = gameObject.GetComponent<PhotonView>();
+        if(view == null) { view = PowerPlayFieldManager.attachView(gameObject); }
+    }
 
     private void Reset()
     {
@@ -21,8 +31,21 @@ public class ReleaseObjectInField : MonoBehaviour
 
     public void Release(int team)
     {
+        if (PhotonNetwork.IsConnected)
+        {
+            view.RPC("ReleaseObj", RpcTarget.All, team);
+        }
+        else
+        {
+            ReleaseObj(team);
+        }
+    }
+
+    [PunRPC]
+    public void ReleaseObj(int team)
+    {
         GameObject obj = Objects[(int)team];
-        if(cloneObject)
+        if (cloneObject)
         {
             obj = Instantiate(Objects[team]);
             obj.GetComponent<ColorSwitcher>().TeamColor_ = (TeamColor)team;
@@ -30,6 +53,11 @@ public class ReleaseObjectInField : MonoBehaviour
         }
         obj.transform.position = Location[(int)team].transform.position;
         obj.GetComponent<Rigidbody>().isKinematic = false;
-    }
 
+        if (PhotonNetwork.IsConnected)
+        {
+            PowerPlayFieldManager.attachView(obj,true);
+        }
+
+    }
 }
