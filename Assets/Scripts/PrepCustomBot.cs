41b847504760ea93b7a3f6a2680becb8b913f81c
ChrisAshtear <chrisashtear@gmail.com>
2023-01-07 19:20:50 -0500

    fixed issues with multiplayer lobby - bot select and game mode

---
+++ Assets/Scripts/PrepCustomBot.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public  class PrepCustomBot: MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    public void PrepBot(string customName)
+    {
+        spawnAddressablePrefab sp = FindObjectOfType<spawnAddressablePrefab>();
+        sp.loadedPrefab.RemoveAllListeners();
+        sp.loadedPrefab.AddListener(BotLoaded);
+        sp.LoadObjMP(customName);
+    }
+
+    public void BotLoaded(GameObject cBot)
+    {
+
+    }
+}
+
