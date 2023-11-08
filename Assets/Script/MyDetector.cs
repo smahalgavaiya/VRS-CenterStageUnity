a32b14179754d55f4d0f5ab437ffba48790aebca
umarirshad126 <umar.irshad@ozaarxr.com>
2023-11-08 20:27:48 +0500

    change Web-Camera to main camera
    April tag Stuff added.

---
+++ Assets/Script/MyDetector.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AprilTag;
+using System.Linq;
+using UnityEngine.UI;
+using TMPro;
+
+public class MyDetector : MonoBehaviour
+{
+    [SerializeField] int _decimation = 4;
+    [SerializeField] float _tagSize = 0.05f;
+    [SerializeField] Material _tagMaterial = null;
+    [SerializeField] RenderTexture _renderTexture = null; // Add your Render Texture field here.
+    [SerializeField] RawImage _webcamPreview = null;
+    [SerializeField] TMP_Text _debugText = null;
+
+    AprilTag.TagDetector _detector;
+    //TagDrawer _drawer;
+
+    // Use a Texture2D to store the texture data.
+    private Texture2D _textureData;
+
+    void Start() {
+        var dims = new Vector2Int(_renderTexture.width, _renderTexture.height); // Use the Render Texture dimensions.
+        _detector = new AprilTag.TagDetector(dims.x, dims.y, _decimation);
+        //_drawer = new TagDrawer(_tagMaterial);
+
+        // Initialize the Texture2D with the dimensions of the Render Texture.
+        _textureData = new Texture2D(_renderTexture.width, _renderTexture.height, TextureFormat.RGBA32, false);
+    }
+
+    void OnDestroy() {
+        _detector.Dispose();
+        //_drawer.Dispose();
+    }
+
+    void LateUpdate() {
+        // Set the Render Texture as the source for _webcamPreview if needed.
+        if(_webcamPreview != null) {
+            _webcamPreview.texture = _renderTexture;
+        }
+
+        // Ensure the Render Texture is active for reading.
+        RenderTexture.active = _renderTexture;
+
+        // Read the pixels from the Render Texture into the Texture2D.
+        _textureData.ReadPixels(new Rect(0, 0, _renderTexture.width, _renderTexture.height), 0, 0);
+        _textureData.Apply();
+
+        // AprilTag detection
+        var fov = Camera.main.fieldOfView * Mathf.Deg2Rad;
+        _detector.ProcessImage(_textureData.GetPixels32(), fov, _tagSize);
+
+        // Detected tag visualization
+        foreach(var tag in _detector.DetectedTags) {
+            //_drawer.Draw(tag.ID, tag.Position, tag.Rotation, _tagSize);
+            Debug.Log(tag.Position);
+        }
+
+        // Profile data output (with 30 frame interval)
+        if(Time.frameCount % 30 == 0)
+            _debugText.text = _detector.ProfileData.Aggregate
+              ("Profile (usec)", (c, n) => $"{c}\n{n.name} : {n.time}");
+    }
+}
