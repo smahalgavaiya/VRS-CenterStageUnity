61ba4face4a56a87befdeb9b0040dedef8b21cd9
umarirshad126 <umar.irshad@ozaarxr.com>
2023-11-15 20:53:23 +0500

    add more tags and add emission when active tag

--- Assets/AprilTag/Script/MyDetector.cs
+++ Assets/AprilTag/Script/MyDetector.cs
@@ -13,9 +13,11 @@ public class MyDetector :MonoBehaviour {
     [SerializeField] Material _tagMaterial = null;
     [SerializeField] RenderTexture _renderTexture = null; // Add your Render Texture field here.
     [SerializeField] RawImage _webcamPreview = null;
-    //[SerializeField] TMP_Text _debugText = null;
+  //[SerializeField] TMP_Text _debugText = null;
     [SerializeField] CameraSwitcher cameraSwitcher;
     [SerializeField] TMP_Text showText;
+    [SerializeField] Color _emissionColor = Color.green;
+    string previousTag = "";
 
 
     AprilTag.TagDetector _detector;
@@ -36,7 +38,7 @@ public class MyDetector :MonoBehaviour {
     private void StartRendering() {
         var dims = new Vector2Int(_renderTexture.width, _renderTexture.height); // Use the Render Texture dimensions.
         _detector = new TagDetector(dims.x, dims.y, _decimation);
-        _drawer = new TagDrawer(_tagMaterial);
+         _drawer = new TagDrawer(_tagMaterial);
 
         // Initialize the Texture2D with the dimensions of the Render Texture.
         _textureData = new Texture2D(_renderTexture.width, _renderTexture.height, TextureFormat.RGBA32, false);
@@ -50,6 +52,8 @@ public class MyDetector :MonoBehaviour {
             if(_aprilTagCamera.tag.Equals("AprilTag")) {
                 StartRendering();
                 _isAprilTagCameraActive = true;
+
+              
             }
         }
         else {
@@ -59,7 +63,7 @@ public class MyDetector :MonoBehaviour {
 
     void OnDestroy() {
         _detector.Dispose();
-       // _drawer.Dispose();
+        _drawer.Dispose();
     }
 
     void LateUpdate() {
@@ -71,23 +75,59 @@ public class MyDetector :MonoBehaviour {
                 _webcamPreview.texture = _renderTexture;
             }
 
+           
+
             // Ensure the Render Texture is active for reading.
             RenderTexture.active = _renderTexture;
-
+            
             // Read the pixels from the Render Texture into the Texture2D.
             _textureData.ReadPixels(new Rect(0, 0, _renderTexture.width, _renderTexture.height), 0, 0);
             _textureData.Apply();
 
             // AprilTag detection
             var fov = _referenceCamera.fieldOfView * Mathf.Deg2Rad;
-            _detector.ProcessImage(_textureData.GetPixels32(), fov, _tagSize);
-
+           _detector.ProcessImage(_textureData.GetPixels32(), fov, _tagSize);
+            
             // Detected tag visualization
-            foreach(var tag in _detector.DetectedTags) {
+            foreach (var tag in _detector.DetectedTags) {
+                
 
                 _drawer.Draw(tag.ID, tag.Position, tag.Rotation, _tagSize);
-                Debug.Log(tag.Position);
-                showText.text = "April Tag Detected :  " + tag.Position.ToString();
+                Debug.Log(tag.ID);
+                Debug.Log("Tag Position : " + tag.Position);
+                showText.text = "April Tag Detected: " + tag.Position.ToString();
+
+               GameObject A = GameObject.FindGameObjectWithTag(tag.ID.ToString());
+            
+
+                if (A != null)
+                {
+                    Renderer renderer = A.GetComponent<Renderer>();
+                    Material material = renderer.material;
+                    material.EnableKeyword("_EMISSION");
+
+                    if (tag.ID.ToString() != previousTag)
+                    {
+                        previousTag = tag.ID.ToString();
+                        // Enable emission
+                        material.EnableKeyword("_EMISSION");
+                    }
+                    //else
+                    //{
+                    //    // Disable emission
+                    //    material.DisableKeyword("_EMISSION");
+                    //}
+
+                    // Update the previousTag for the next iteration
+              
+                    
+                    Debug.Log("Privious tag is" + previousTag);
+                }
+                else
+                {
+                    Debug.LogError("GameObject with tag 'tag.ID' not found.");
+                }
+                
 
             }
 
